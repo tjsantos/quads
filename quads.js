49e71623797ff0074f7d108db3cd549cbae0c5f9
T.J. Santos <tjscode@outlook.com>
2015-09-05 04:46:48 -0700

    Implement threshold variance for drawing

--- quads.js
+++ quads.js
@@ -1,10 +1,4 @@
-// TODO
-// "image loading..." text while loading
-// center image and ui
-// resizable canvas
-// dataURL view image slow ??
-// quads stuff
-// canvas layers?
+
 /*
 if (document.readyState != 'loading') {
     init();
@@ -17,53 +11,51 @@ var canvas = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
 
 function AreaNode(dx, dy, dw, dh, depth) {
-    // TODO validate input
     this.dx = dx;
     this.dy = dy;
     this.dw = dw;
     this.dh = dh;
     this.depth = depth;
+    this.meanRGB = integralImage.meanRGB(dx, dy, dw, dh);
+    this.varRGB = integralImage.varRGB(dx, dy, dw, dh);
 }
 AreaNode.prototype.children = function() {
-    if (this.dw == 1 && this.dh == 1) {
-        return [];
+    if (!this._children) {
+        if (this.dw == 1 && this.dh == 1) {
+            this._children = [];
+        } else {
+            // split into up to 4 quadrants with dimensions:
+            // w1 x h1 | w2 x h1
+            // w1 x h2 | w2 x h2
+            // w1 or h1 may be zero
+            var w1 = Math.floor(this.dw / 2);
+            var w2 = this.dw - w1;
+            var h1 = Math.floor(this.dh / 2);
+            var h2 = this.dh - h1;
+            var newDepth = this.depth + 1;
+            var children = [];
+            // top left, top right, bottom left, bottom right
+            if (w1 > 0 && h1 > 0) {
+                children.push(new AreaNode(this.dx, this.dy, w1, h1, newDepth));
+            }
+            if (h1 > 0) {
+                children.push(new AreaNode(this.dx + w1, this.dy, w2, h1, newDepth));
+            }
+            if (w1 > 0) {
+                children.push(new AreaNode(this.dx, this.dy + h1, w1, h2, newDepth));
+            }
+            children.push(new AreaNode(this.dx + w1, this.dy + h1, w2, h2, newDepth));
+            this._children = children;
+        }
     }
 
-    // split into up to 4 quadrants with dimensions:
-    // w1 x h1 | w2 x h1
-    // w1 x h2 | w2 x h2
-    // w1 or h1 may be zero
-    var w1 = Math.floor(this.dw / 2);
-    var w2 = this.dw - w1;
-    var h1 = Math.floor(this.dh / 2);
-    var h2 = this.dh - h1;
-    var newDepth = this.depth + 1;
-    var children = [];
-    // top left, top right, bottom left, bottom right
-    if (w1 > 0 && h1 > 0) {
-        children.push(new AreaNode(this.dx, this.dy, w1, h1, newDepth));
-    }
-    if (h1 > 0) {
-        children.push(new AreaNode(this.dx + w1, this.dy, w2, h1, newDepth));
-    }
-    if (w1 > 0) {
-        children.push(new AreaNode(this.dx, this.dy + h1, w1, h2, newDepth));
-    }
-    children.push(new AreaNode(this.dx + w1, this.dy + h1, w2, h2, newDepth));
-
-    //console.log('p: ' + [this.dx,this.dy,this.dw,this.dh,this.depth].join());
-    //children.forEach(function(a) {
-    //    console.log('c: ' + [a.dx, a.dy, a.dw, a.dh, a.depth].join());
-    //});
-    return children;
+    return this._children;
 };
 
 function IntegralImage(imageData) {
     this.imageData = imageData;
-    // TODO: pre-calculate area sums
 }
-IntegralImage.prototype.avg = function(dx, dy, dw, dh) {
-    // TODO validate input
+IntegralImage.prototype._sumRGB = function(dx, dy, dw, dh) {
     var sumR = 0;
     var sumG = 0;
     var sumB = 0;
@@ -75,60 +67,60 @@ IntegralImage.prototype.avg = function(dx, dy, dw, dh) {
             sumG += data[i + 1];
             sumB += data[i + 2];
         }
-    var avgRGB = [sumR, sumG, sumB].map(function(x) {
-        return Math.floor(x / (dw * dh));
+    return [sumR, sumG, sumB];
+};
+IntegralImage.prototype._sumRGB2 = function(dx, dy, dw, dh) {
+    var sumR2 = 0;
+    var sumG2 = 0;
+    var sumB2 = 0;
+    var data = this.imageData.data;
+    for (var y = dy; y < dy + dh; y += 1)
+        for (var x = dx; x < dx + dw; x += 1) {
+            var i = 4*y*this.imageData.width + 4*x;
+            sumR2 += Math.pow(data[i], 2);
+            sumG2 += Math.pow(data[i + 1], 2);
+            sumB2 += Math.pow(data[i + 2], 2);
+        }
+    return [sumR2, sumG2, sumB2];
+};
+IntegralImage.prototype.meanRGB = function(dx, dy, dw, dh) {
+    var n = dw * dh;
+    return this._sumRGB(dx, dy, dw, dh).map(function(x) {
+        return Math.round(x/n);
     });
-    return avgRGB;
 };
-
-function Queue() {
-    // two stack implementation
-    this.enter = [];
-    this.exit = [];
-}
-Queue.prototype.enqueue = function(item) {
-    this.enter.push(item);
-};
-Queue.prototype._prepExit = function() {
-    if (this.exit.length > 0) return;
-
-    while (this.enter.length > 0) {
-        this.exit.push(this.enter.pop());
+IntegralImage.prototype.varRGB = function(dx, dy, dw, dh) {
+    var n = dw * dh;
+    var sumRGB = this._sumRGB(dx, dy, dw, dh);
+    var sumRGB2 = this._sumRGB2(dx, dy, dw, dh);
+    var result = [];
+    for (var i = 0; i < sumRGB.length; i++) {
+        // E[X^2] - E[X]^2
+        var varX = (sumRGB2[i] / n) - Math.pow(sumRGB[i]/n, 2);
+        result.push(varX);
     }
+    return result;
 };
-Queue.prototype.dequeue = function() {
-    if (this.length === 0) throw new Error('queue is empty');
-
-    this._prepExit();
-    return this.exit.pop();
-};
-Queue.prototype.peek = function() {
-    if (this.length === 0) throw new Error('queue is empty');
-
-    this._prepExit();
-    return this.exit[this.exit.length - 1];
-};
-Object.defineProperty(Queue.prototype, 'length', {
-    get: function() {return this.enter.length + this.exit.length;}
-});
 
 var draw;
 var n;
-var queue;
 var start;
 var prevTimestamp;
 var npms = 5;
 var depthPerSec = 1;
 var intervals = [];
-var nodes = [];
+var nodes;
+var threshold = 5000;
+var orig;
+var integralImage;
 function begin() {
-    var orig = context.getImageData(0, 0, canvas.width, canvas.height);
-    var integralImage = new IntegralImage(orig);
+    orig = context.getImageData(0, 0, canvas.width, canvas.height);
+    integralImage = new IntegralImage(orig);
     n = 0;
-    queue = new Queue();
-    queue.enqueue(new AreaNode(0, 0, canvas.width, canvas.height, 0));
+    nodes = [new AreaNode(0, 0, canvas.width, canvas.height, 0)];
+
     draw = function draw(timestamp) {
-        if (queue.length == 0) {
+        if (n === nodes.length) {
             var actualNps = (n / (timestamp - start)) * 1000;
             console.log(actualNps);
             return;
@@ -137,20 +129,24 @@ function begin() {
         var interval = timestamp - prevTimestamp;
         intervals.push(interval);
         prevTimestamp = timestamp;
-        var depth = queue.peek().depth;
-        if ((timestamp - start)*depthPerSec/1000 >= depth) {
-            //for (var i = 0; i < interval * npms && queue.length > 0; i++) {
-            while (queue.length && queue.peek().depth === depth) {
-                var a = queue.dequeue();
-                var avgRGB = integralImage.avg(a.dx, a.dy, a.dw, a.dh);
-                context.fillStyle = 'rgb(' + avgRGB.join(',') + ')';
-                context.fillRect(a.dx, a.dy, a.dw, a.dh);
-                a.children().forEach(function (a) {
-                    queue.enqueue(a);
-                });
+
+        // draw nodes by depth
+        var currentDepth = nodes[n].depth;
+        if ((timestamp - start)*depthPerSec/1000 >= currentDepth) {
+            while (n < nodes.length && nodes[n].depth === currentDepth) {
+                var node = nodes[n];
+                var nodeVarTotal = node.varRGB.reduce(function(a, b) { return a + b; });
+                if (nodeVarTotal > threshold) {
+                    node.children().forEach(function (a) {
+                        context.fillStyle = 'rgb(' + a.meanRGB.join(',') + ')';
+                        context.fillRect(a.dx, a.dy, a.dw, a.dh);
+                        nodes.push(a);
+                    });
+                }
                 n += 1;
             }
         }
+
         requestAnimationFrame(draw);
     };
     prevTimestamp = start = performance.now();
@@ -159,7 +155,6 @@ function begin() {
 
 
 function imageToCanvas() {
-    console.log('drawing image to canvas...');
     canvas.width = img.width;
     canvas.height = img.height;
     context.drawImage(img, 0, 0);
@@ -178,3 +173,24 @@ function readImage() {
 }
 var input = document.querySelector('#fileInput');
 input.addEventListener('change', readImage);
+
+function timeFillRect(r, g, b) {
+    var start = performance.now();
+    var id = context.createImageData(1, 1);
+    var d = id.data;
+    for (var x = 0; x < canvas.width; x++) {
+        for (var y = 0; y < canvas.height; y++) {
+            /*
+            d[0] = r;
+            d[1] = g;
+            d[2] = b;
+            d[3] = 255;
+            context.putImageData(id, x, y);
+            */
+            context.fillStyle = 'rgb(' + [r,g,b].join(',') + ')';
+            context.fillRect(x, y, 1, 1);
+        }
+    }
+    var end = performance.now();
+    console.log(start, end, end - start);
+}
